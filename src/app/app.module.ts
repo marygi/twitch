c1fd80207c1395bfe7df66db42d3630c2fb51de5
Marina <marina.g.ivanova@gmail.com>
2020-02-25 22:46:43 +0200

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+
 
 @NgModule({
 	declarations: [
@@ -14,7 +16,8 @@ import { DashboardModule } from './dashboard/dashboard.module';
 		BrowserModule,
 		CommonModule,
 		AppRoutingModule,
-		DashboardModule
+		DashboardModule,
+		BrowserAnimationsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
