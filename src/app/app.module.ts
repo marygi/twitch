ebed2f755d20e843b7d47e4aebc56bc2687ecfbb
Marina <marina.g.ivanova@gmail.com>
2020-02-26 13:22:14 +0200

    header component / logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { SharedModule } from './shared/shared.module';
 import { LoginModule } from './login/login.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		HeaderComponent,
 	],
 	imports: [
 		BrowserModule,
