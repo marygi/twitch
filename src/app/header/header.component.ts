2ef215cdef5bf913e8a9dcae052770e4422d4daf
Marina <marina.g.ivanova@gmail.com>
2020-02-27 09:57:57 +0200

    added user data to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { UserService } from '../shared/service/user.service';
+import { User } from '../shared/model/user';
 
 @Component({
 	selector: 'app-header',
@@ -7,14 +11,19 @@ import {Router} from '@angular/router';
 	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+	user: User;
+	subscription: Subscription;
 
-	constructor(private router: Router) { }
+	constructor(private router: Router,
+				private userService: UserService) { }
 
 	ngOnInit() {
+		this.subscription = this.userService.getUser().subscribe(user => this.user = user);
 	}
 
 	logout() {
 		localStorage.removeItem('token');
+		this.userService.clearUser();
 		this.router.navigate(['/']);
 	}
 
